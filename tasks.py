568ae45c5dd02c14ce552e920b9f75949f0a138b
Prisacari Dmitrii <mrgangs1991@gmail.com>
2019-10-17 17:48:05 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -5,24 +5,14 @@ from app import app
 
 
 class CreateListTask(Task):
-    name_plain = 'create_list'
-    namespace = 'ns'
-
-    @property
-    def name(self):
-        return self.namespace + ':' + self.name_plain
+    name = 'create_list'
 
     def run(self, *args, **kwargs):
         return ['google.com']
 
 
 class PrintListWithNumberTask(Task):
-    name_plain = 'print_list_with_number'
-    namespace = 'ns'
-
-    @property
-    def name(self):
-        return self.namespace + ':' + self.name_plain
+    name = 'print_list_with_number'
 
     def run(self, the_list, number):
         logging.info(str(the_list) + ' ' + str(number + 1))
